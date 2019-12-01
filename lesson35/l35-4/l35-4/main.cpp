ece9992d8a41949079b51c8ecb06757aeb50c1d6
Dmitry Kanaev <dimmkan@mail.ru>
2019-12-01 13:51:53 +0300

    Урок 35 - задача 4

--- lesson35/l35-4/l35-4/main.cpp
+++ lesson35/l35-4/l35-4/main.cpp
@@ -10,16 +10,20 @@ int main()
 
     char ch;
     int count = 0;
-    while(in >> ch){
-        count++;
-    }
-    in.seekg(0, ios_base::beg);
+    in.seekg(0, ios::end);
+    count = in.tellg();
+    in.seekg(0, ios::beg);
     for(int i = 0; i<count/2; i++){
-        in.seekg(i, ios_base::beg);
-        in >> ch;
+        in.seekg(i, ios::beg);
+        in.get(ch);
         out << ch;
-        in.seekg(-i-1, ios_base::end);
-        in >> ch;
+        in.seekg(-i-1, ios::end);
+        in.get(ch);
+        out << ch;
+    }
+    if(count%2 == 1){
+        in.seekg(count/2, ios::beg);
+        in.get(ch);
         out << ch;
     }
     in.close();
