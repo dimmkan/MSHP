520e8d51ac9702e706acf9279d2fe3fa04f63138
Dmitry <dimmkan@mail.ru>
2019-12-01 11:24:06 +0300

    Урок 34 задача 8

--- lesson34/l34-8/l34-8/main.cpp
+++ lesson34/l34-8/l34-8/main.cpp
@@ -5,16 +5,33 @@ using namespace std;
 
 int main()
 {
-    fstream in;
-    fstream out;
-    in.open("in.txt", ios::in);
-    out.open("out.txt", ios::out);
-    char ch[10001] = {};
-    while (in.getline(ch, 10001, '\n')) {
-        out << ch;
-        out.seekp(strlen(ch)-1, ios::cur);
-        out << endl;
+    fstream in("in.txt", ios::in);
+    fstream out("out.txt", ios::out);
+
+    char **data = new char*[102400];
+
+    int i = 0;
+    while(!in.eof()){
+        char s[102401] = {};
+        in.getline(s, 102401);
+        int len = strlen(s)+1;
+        data[i] = new char[len];
+        for(int j = 0; j < len; j++){
+            data[i][j] = 0; // '\0'
+        }
+        strncpy(data[i++], s, len-1);
     }
+
+    for(int j = i-2; j >= 0; j--){
+        out << data[j] << endl;
+    }
+
+    for(int j = 0; j < i; j++){
+        delete [] data[j];
+    }
+
+    delete [] data;
+
     in.close();
     out.close();
     return 0;
