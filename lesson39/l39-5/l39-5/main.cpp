350a52d6795736b95bb4835671a3ab4a4f6a5949
Dmitry Kanaev <dimmkan@mail.ru>
2020-01-08 14:18:10 +0300

    Урок 39 - задача 5 (Симулятор студента - уровни сложности)

---
+++ lesson39/l39-5/l39-5/main.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+using namespace std;
+
+class Student {
+    char name[51];
+    char surname[51];
+    int hp;
+    int energy;
+    int iq;
+    int sanity;
+    int excitement;
+
+    void change_field(int&, int);
+    void change_params(int hp, int energy, int iq, int sanity, int excitement);
+    void apply_effects();
+public:
+    Student(int hp = 100, int energy = 100, int iq = 100, int sanity = 20, int excitement = 50);
+    Student(char* name, char* surname, int hp = 100, int energy = 100, int iq = 100, int sanity = 20, int excitement = 50);
+    bool is_alive();
+    void show();
+    void eat();
+    void wait();
+    void study();
+    void sleep();
+    void watch_tv();
+};
+
+Student::Student(int hp, int energy, int iq, int sanity, int excitement)
+{
+    this->hp = hp;
+    this->energy = energy;
+    this->iq = iq;
+    this->sanity = sanity;
+    this->excitement = excitement;
+}
+
+Student::Student(char* name, char* surname, int hp, int energy, int iq, int sanity, int excitement):Student(hp, energy, iq, sanity, excitement)
+{
+    strcpy(this->name, name);
+    strcpy(this->surname, surname);
+}
+
+bool Student::is_alive() {
+    return hp > 0;
+}
+
+void Student::show() {
+    cout << name << ' ' << surname
+         << ": HP = " << setw(3) << setfill('0') << hp
+         << ", Energy = " << setw(3) << setfill('0') << energy
+         << ", IQ = " << setw(3) << setfill('0') << iq
+         << ", Sanity = " << setw(3) << setfill('0') << sanity
+         << ", Excitement = " << setw(3) << setfill('0') << excitement
+         << '.';
+    if (!is_alive())
+        cout << " Game over.";
+    cout << endl;
+}
+
+
+void Student::change_field(int &field, int delta) {
+    field += delta;
+    if (field < 0) field = 0;
+    if (field > 100) field = 100;
+}
+
+void Student::change_params(int hp, int energy, int iq, int sanity, int excitement){
+    change_field(this->hp, hp);
+    change_field(this->energy, energy);
+    change_field(this->iq, iq);
+    change_field(this->sanity, sanity);
+    change_field(this->excitement, excitement);
+}
+
+void Student::apply_effects(){
+    hp -= (energy == 0 ? 25 : 0);
+    hp -= (energy == 100 ? 10 : 0);
+    hp -= (sanity <= 10 ? 15-sanity : 0);
+    sanity -= (excitement < 25 ? 5 : 0);
+    change_params(0, 0, 0, 0, 0);
+}
+
+void Student::eat() {
+    if (!is_alive()) return;
+    change_params(1, 7, -1, 0, -2);
+    apply_effects();
+}
+
+void Student::wait() {
+    if (!is_alive()) return;
+    change_params(1, -3, 0, 0, -3);
+    apply_effects();
+}
+
+void Student::study() {
+    if (!is_alive()) return;
+    change_params(-2, -4, 5, -5, -2);
+    apply_effects();
+}
+
+void Student::sleep() {
+    if (!is_alive()) return;
+    change_params(2, -2, 0, 7, 0);
+    apply_effects();
+}
+
+void Student::watch_tv() {
+    if (!is_alive()) return;
+    change_params(-2, -3, -3, 1, 5);
+    apply_effects();
+}
+
+int main() {
+    char name[51], surname[51];
+    cin >> name >> surname;
+    int hp, energy, iq, sanity, excitement;
+    cin >> hp >> energy >> iq >> sanity >> excitement;
+    Student character(name, surname, hp, energy, iq, sanity, excitement);
+    int m;
+    char cmd[50];
+    cin >> m;
+    for (int i = 0; i < m; ++i) {
+        cin >> cmd;
+        if (!strcmp(cmd, "Wait"))
+            character.wait();
+        else if (!strcmp(cmd, "Eat"))
+            character.eat();
+        else if (!strcmp(cmd, "Study"))
+            character.study();
+        else if (!strcmp(cmd, "Sleep"))
+            character.sleep();
+        else if (!strcmp(cmd, "Watch")) {
+            character.watch_tv();
+            cin >> cmd;
+        } else
+            return 1;
+        character.show();
+    }
+    return 0;
+}
