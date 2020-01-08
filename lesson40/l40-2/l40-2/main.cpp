34ca7351d30be90e4d6581f3c8480f3dcdcb7672
Dmitry Kanaev <dimmkan@mail.ru>
2020-01-08 16:44:17 +0300

    Урок 40 - задача 2 (День рождения Артема - 2)

---
+++ lesson40/l40-2/l40-2/main.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+
+using namespace std;
+
+class CandyBox{
+    int cnt;
+    int *candies;
+public:
+    CandyBox();
+    CandyBox(const CandyBox &tmp);
+    CandyBox &operator=(CandyBox);
+    ~CandyBox();
+
+    void read();
+    void print();
+    int varieties();
+    int size();
+    int& at(int index);
+};
+
+bool in(int *data, int size, int value){
+    for(int i = 0; i < size; i++){
+        if(data[i] == value){
+            return true;
+        }
+    }
+    return false;
+}
+
+CandyBox::CandyBox(){
+    this->candies = nullptr;
+    this->cnt = 0;
+}
+
+CandyBox::CandyBox(const CandyBox &tmp){
+    this->cnt = tmp.cnt;
+    this->candies = new int[this->cnt];
+    for(int i = 0; i < this->cnt; i++){
+        this->candies[i] = tmp.candies[i];
+    }
+}
+
+CandyBox &CandyBox::operator=(CandyBox tmp){
+    this->cnt = tmp.cnt;
+    this->candies = new int[this->cnt];
+    for(int i = 0; i < this->cnt; i++){
+        this->candies[i] = tmp.candies[i];
+    }
+    return *this;
+}
+
+CandyBox::~CandyBox(){
+    delete [] this->candies;
+}
+
+void CandyBox::read(){
+    cin >> this->cnt;
+    if(this->candies){
+        delete [] this->candies;
+    }
+    this->candies = new int[this->cnt];
+    for(int i = 0; i < this->cnt; i++){
+        cin >> this->candies[i];
+    }
+}
+
+void CandyBox::print(){
+    for(int i = 0; i < this->cnt; i++){
+        cout << this->candies[i] << " ";
+    }
+    cout << endl;
+}
+
+int CandyBox::varieties(){
+    int *data = new int[this->cnt];
+    int count = 0;
+    for(int i = 0; i < this->cnt; i++){
+        if(!in(data, count, this->candies[i])){
+            data[count++] = this->candies[i];
+        }
+    }
+    delete [] data;
+    return count;
+}
+
+int CandyBox::size(){
+    return this->cnt;
+}
+
+int& CandyBox::at(int index){
+    return this->candies[index];
+}
+
+CandyBox Arcady_process(CandyBox b){
+    CandyBox c = b;
+    int *data = new int[c.size()];
+    int count = 0, i;
+    for(i = 0; i < c.size(); i++){
+        if(c.at(i)%2 == 0){
+            data[count++] = c.at(i);
+            c.at(i) = 0;
+        }
+    }
+    for(i = 1; i < count-2; i+=2){
+        data[i] = data[i+1];
+    }
+    count = i-1;
+    for(i = 0; i < count; i++){
+        int j = 0;
+        while(j < c.size()){
+            if(c.at(j) == 0){
+                c.at(j) = data[i];
+                break;
+            }
+            j++;
+        }
+    }
+    return c;
+}
+
+CandyBox Alice_process(CandyBox b){
+    CandyBox c = b;
+    int *data = new int[c.size()-c.size()/2];
+    int count = 0;
+    for(int i = c.size()/2; i < c.size(); i++){
+        data[count++] = c.at(i);
+    }
+    for(int i = 0; i < c.size(); i++){
+        c.at(i) = 0;
+    }
+    count = 0;
+    for(int i = 0; i < c.size(); i+=2){
+        c.at(i) = data[count++];
+    }
+    return c;
+}
+
+int main()
+{
+    CandyBox a, b, c;
+    c.read();
+    a = Arcady_process(c);
+    b = Alice_process(c);
+    a.print();
+    b.print();
+    return 0;
+}
