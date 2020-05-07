06d2e6d970d0e5396ef94615c0b254c21b12f69e
Dmitry Kanaev <dimmkan@mail.ru>
2020-05-07 19:20:28 +0300

    Урок 53 - STL::algorithm - Разложение на простые множители

---
+++ lesson53/l53-7/l53-7/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef vector <int> v_int;
+typedef istream_iterator<int> in_it;
+typedef ostream_iterator<int> out_it;
+
+void recursive_simple(v_int &v, int n, int div){
+    if (n == 1) return;
+      if (n%div == 0)
+      {
+        v.push_back(div);
+        recursive_simple(v, n / div, div);
+      }
+      else
+        if (div == 2)
+          recursive_simple(v, n, div + 1);
+        else
+          recursive_simple(v, n, div + 2);
+}
+
+int main()
+{    
+    int n;
+    cin >> n;
+    v_int v;
+    recursive_simple(v, n, 2);
+    auto vEnd = unique(v.begin(), v.end());
+    copy(v.begin(), vEnd, out_it(cout, " "));
+    return 0;
+}
