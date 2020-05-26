a5c8e7de5d366e2881aa28d88218c5035be0b31c
Dmitry Kanaev <dimmkan@mail.ru>
2020-05-26 15:25:43 +0300

    Урок 54 - STL::algorithm - Соседи А

--- lesson54/l54-6/l54-6/main.cpp
+++ lesson54/l54-6/l54-6/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+typedef vector <int> v_int;
+typedef istream_iterator<int> in_it;
+typedef ostream_iterator<int> out_it;
+
+
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    v_int v;
+    int n, a;
+    cin >> n >> a;
+    copy_n(in_it(cin), n, back_inserter(v));
+    auto f = find_if(v.begin(), v.end(), [a](int val){
+        return val > a;
+    });
+    cout << f - v.begin() << " " << *(f-1) << endl;
+    cout << f + 1 - v.begin() << " " << *f;
     return 0;
 }
